ad0ad9fc2c3b60471a8c9322363acc5344d7fd81
biomotion <bensonpuppy1997@gmail.com>
2020-04-29 15:56:13 +0800

    enlarge crop size

--- catkin_ws/src/icp_localization/src/ICPManager.cpp
+++ catkin_ws/src/icp_localization/src/ICPManager.cpp
@@ -48,9 +48,8 @@ void ICPManager::feedPC(pcl::PointCloud<pcl::PointXYZ>& input_cloud){
         icp.setInputTarget(map_cloud);
     else{
         Eigen::Vector3f point = gs.topRightCorner(3, 1);
-        // std::cout << selectMapRange(point(0), point(1), point(2), 10, 10, 10)->width << std::endl;
         pcl::PointCloud<PointXYZ>::Ptr inputTarget(new pcl::PointCloud<pcl::PointXYZ>);
-        this->selectMapRange(point(0), point(1), point(2), 20, 20, 20, inputTarget);
+        this->selectMapRange(point(0), point(1), point(2), 50, 50, 50, inputTarget);
         icp.setInputTarget(inputTarget);
     }
     std::cout << "aligning..." << std::endl;
