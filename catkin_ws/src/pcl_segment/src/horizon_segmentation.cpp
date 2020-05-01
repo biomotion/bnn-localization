a2a7cfb553fe2afb98849d35053d7a5420456754
leohsu870325 <leohsu210200@gmail.com>
2020-05-01 15:51:57 +0800

    Version2 pcl segmentation package (pcl_segment)

--- catkin_ws/src/pcl_segment/src/horizon_segmentation.cpp
+++ catkin_ws/src/pcl_segment/src/horizon_segmentation.cpp
@@ -2,8 +2,8 @@
 # include <pcl/io/pcd_io.h>
 # include <pcl/point_types.h>
 # include <pcl/filters/passthrough.h>
-// # include <pcl/point_cloud.h>
 # include <pcl_conversions/pcl_conversions.h>
+// # include <pcl/point_cloud.h>
 // # include <pcl/filters/voxel_grid.h>
 
 # include <sensor_msgs/PointCloud2.h>
@@ -29,20 +29,16 @@ void pcl_lower_segmentation(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &inpu
      pcl::PassThrough<pcl::PointXYZ> pass;
      pass.setInputCloud(input_cloud);
      pass.setFilterFieldName("z");
-     pass.setFilterLimits(__FLT_MIN__, 0.8);
+     // pass.setFilterLimits(__FLT_MIN__, 0.8);
+     pass.setFilterLimits(-100, 0.8);
      // pass.setFilterLimitsNegative (true);
      pass.filter(*cloud_filtered);
-
-     // std::cerr << "Cloud after filtering: " << std::endl;
-     // for (std::size_t i = 0; i < cloud_filtered->points.size(); ++i)
-     //      std::cerr << "    " << cloud_filtered->points[i].x << " "
-     //                << cloud_filtered->points[i].y << " "
-     //                << cloud_filtered->points[i].z << std::endl;
 // filtering a PointCloud using a PassThrough filter%
 
 // processing data type & publish
      pcl::toPCLPointCloud2(*cloud_filtered, cloud_lower_output);
      pcl_conversions::fromPCL(cloud_lower_output, msg_lower_output);
+     msg_lower_output.header.frame_id = "/velodyne";
      pub_lower.publish(msg_lower_output);
 // processing data type & publish%
 
@@ -52,8 +48,8 @@ void pcl_mid_segmentation(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &input_
 
 // variable claiming
      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
-     pcl::PCLPointCloud2 cloud_lower_output;
-     sensor_msgs::PointCloud2 msg_lower_output;
+     pcl::PCLPointCloud2 cloud_mid_output;
+     sensor_msgs::PointCloud2 msg_mid_output;
 // variable claiming%
 
 // filtering a PointCloud using a PassThrough filter
@@ -64,18 +60,13 @@ void pcl_mid_segmentation(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &input_
      pass.setFilterLimits(0.8, 1.8);
      //pass.setFilterLimitsNegative (true);
      pass.filter(*cloud_filtered);
-
-     // std::cerr << "Cloud after filtering: " << std::endl;
-     // for (std::size_t i = 0; i < cloud_filtered->points.size(); ++i)
-     //      std::cerr << "    " << cloud_filtered->points[i].x << " "
-     //                << cloud_filtered->points[i].y << " "
-     //                << cloud_filtered->points[i].z << std::endl;
 // filtering a PointCloud using a PassThrough filter%
 
 // processing data type & publish
-     pcl::toPCLPointCloud2(*cloud_filtered, cloud_lower_output);
-     pcl_conversions::fromPCL(cloud_lower_output, msg_lower_output);
-     pub_lower.publish(msg_lower_output);
+     pcl::toPCLPointCloud2(*cloud_filtered, cloud_mid_output);
+     pcl_conversions::fromPCL(cloud_mid_output, msg_mid_output);
+     msg_mid_output.header.frame_id = "/velodyne";
+     pub_mid.publish(msg_mid_output);
 // processing data type & publish%
 
 }
@@ -84,8 +75,8 @@ void pcl_upper_segmentation(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &inpu
      
 // variable claiming
      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
-     pcl::PCLPointCloud2 cloud_lower_output;
-     sensor_msgs::PointCloud2 msg_lower_output;
+     pcl::PCLPointCloud2 cloud_upper_output;
+     sensor_msgs::PointCloud2 msg_upper_output;
 // variable claiming%
 
 // filtering a PointCloud using a PassThrough filter
@@ -93,64 +84,39 @@ void pcl_upper_segmentation(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &inpu
      pcl::PassThrough<pcl::PointXYZ> pass;
      pass.setInputCloud(input_cloud);
      pass.setFilterFieldName("z");
-     pass.setFilterLimits(1.8, __FLT_MAX__);
+     // pass.setFilterLimits(1.8, __FLT_MAX__);
+     pass.setFilterLimits(1.8, 100);
      //pass.setFilterLimitsNegative (true);
      pass.filter(*cloud_filtered);
-
-     // std::cerr << "Cloud after filtering: " << std::endl;
-     // for (std::size_t i = 0; i < cloud_filtered->points.size(); ++i)
-     //      std::cerr << "    " << cloud_filtered->points[i].x << " "
-     //                << cloud_filtered->points[i].y << " "
-     //                << cloud_filtered->points[i].z << std::endl;
 // filtering a PointCloud using a PassThrough filter%
 
 // processing data type & publish
-     pcl::toPCLPointCloud2(*cloud_filtered, cloud_lower_output);
-     pcl_conversions::fromPCL(cloud_lower_output, msg_lower_output);
-     pub_lower.publish(msg_lower_output);
+     pcl::toPCLPointCloud2(*cloud_filtered, cloud_upper_output);
+     pcl_conversions::fromPCL(cloud_upper_output, msg_upper_output);
+     msg_upper_output.header.frame_id = "/velodyne";
+     pub_upper.publish(msg_upper_output);
 // processing data type & publish%
 
 }
 
 void horizonSegment_callBack(const sensor_msgs::PointCloud2ConstPtr &input_msg)
 {
-// read point cloud data from PCD files
+     pcl::PCLPointCloud2 pcl_pc2;
+     pcl_conversions::toPCL(*input_msg, pcl_pc2);
      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-
-     //* load the file
-     if (pcl::io::loadPCDFile<pcl::PointXYZ>("../../bags/map.pcd", *cloud) == -1) 
-     {
-          PCL_ERROR("Couldn't read file map.pcd \n");
-          return;
-     }
-     std::cout << "Loaded "
-               << "width: " << cloud->width << ", height" << cloud->height << "\n"
-               << cloud->width * cloud->height
-               << " data points from map.pcd with the following fields: "
-               << std::endl;
-     // for (std::size_t i = 0; i < cloud->points.size(); ++i)
-     //      std::cout << "    " << cloud->points[i].x
-     //                << " " << cloud->points[i].y
-     //                << " " << cloud->points[i].z << std::endl;
-// read point cloud data from PCD files%
+     pcl::fromPCLPointCloud2(pcl_pc2, *cloud);
 
 // Fill in the cloud data
-     cloud->width = 5248917;
-     cloud->height = 1;
-     cloud->points.resize(cloud->width * cloud->height);
-
-     for (std::size_t i = 0; i < cloud->points.size(); ++i)
-     {
-          cloud->points[i].x = 1024 * rand() / (RAND_MAX + 1.0f);
-          cloud->points[i].y = 1024 * rand() / (RAND_MAX + 1.0f);
-          cloud->points[i].z = 1024 * rand() / (RAND_MAX + 1.0f);
-     }
+     // cloud->width = 5248917;
+     // cloud->height = 1;
+     // cloud->points.resize(cloud->width * cloud->height);
 // Fill in the cloud data%
 
 // call segmentation functions
      pcl_lower_segmentation(cloud);
      pcl_mid_segmentation(cloud);
      pcl_upper_segmentation(cloud);
+// call segmentation functions%
 
 }
 
@@ -159,7 +125,7 @@ int main(int argc, char **argv)
      ros::init(argc, argv, "pcl_horizonal_segmentation");
      ros::NodeHandle n;
 
-     sub = n.subscribe("/map", 1, horizonSegment_callBack);
+     sub = n.subscribe("/lidar_points", 1, horizonSegment_callBack);
      pub_lower = n.advertise<sensor_msgs::PointCloud2>("/lower_points", 1);
      pub_mid = n.advertise<sensor_msgs::PointCloud2>("/mid_points", 1);
      pub_upper = n.advertise<sensor_msgs::PointCloud2>("/upper_points", 1);
@@ -167,70 +133,51 @@ int main(int argc, char **argv)
 // for debugging
 
 // read point cloud data from PCD files
-     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+     // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-     //* load the file
-     if (pcl::io::loadPCDFile<pcl::PointXYZ>("../../bags/map.pcd", *cloud) == -1) 
-     {
-          PCL_ERROR("Couldn't read file map.pcd \n");
-          return (-1);
-     }
-     std::cout << "Loaded "
-               << "width: " << cloud->width << ", height" << cloud->height << "\n"
-               << cloud->width * cloud->height
-               << " data points from map.pcd with the following fields: "
-               << std::endl;
-     // for (std::size_t i = 0; i < cloud->points.size(); ++i)
-     //      std::cout << "    " << cloud->points[i].x
-     //                << " " << cloud->points[i].y
-     //                << " " << cloud->points[i].z << std::endl;
+     // //* load the file
+     // if (pcl::io::loadPCDFile<pcl::PointXYZ>("/bags/itri/map.pcd", *cloud) == -1) 
+     // {
+     //      PCL_ERROR("Couldn't read file map.pcd \n");
+     //      return (-1);
+     // }
+     // std::cout << "Loaded "
+     //           << "width: " << cloud->width << ", height" << cloud->height << "\n"
+     //           << cloud->width * cloud->height
+     //           << " data points from map.pcd with the following fields: "
+     //           << std::endl;
+     // // for (std::size_t i = 0; i < cloud->points.size(); ++i)
+     // //      std::cout << "    " << cloud->points[i].x
+     // //                << " " << cloud->points[i].y
+     // //                << " " << cloud->points[i].z << std::endl;
 // read point cloud data from PCD files%
 
-// Fill in the cloud data
-     cloud->width = 5248917;
-     cloud->height = 1;
-     cloud->points.resize(cloud->width * cloud->height);
-
-     for (std::size_t i = 0; i < cloud->points.size(); ++i)
-     {
-          cloud->points[i].x = 1024 * rand() / (RAND_MAX + 1.0f);
-          cloud->points[i].y = 1024 * rand() / (RAND_MAX + 1.0f);
-          cloud->points[i].z = 1024 * rand() / (RAND_MAX + 1.0f);
-     }
-
-     // std::cerr << "Cloud before filtering: " << std::endl;
-     // for (std::size_t i = 0; i < cloud->points.size(); ++i)
-     //      std::cerr << "    " << cloud->points[i].x << " "
-     //                << cloud->points[i].y << " "
-     //                << cloud->points[i].z << std::endl;
-// Fill in the cloud data%
-
 // filtering a PointCloud using a PassThrough filter
-     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
-     pcl::PCLPointCloud2 cloud_lower_output;
-     pcl::PCLPointCloud2 cloud_mid_output;
-     pcl::PCLPointCloud2 cloud_upper_output;
-     sensor_msgs::PointCloud2 msg_lower_output;
-     sensor_msgs::PointCloud2 msg_mid_output;
-     sensor_msgs::PointCloud2 msg_upper_output;
+     // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+     // pcl::PCLPointCloud2 cloud_lower_output;
+     // pcl::PCLPointCloud2 cloud_mid_output;
+     // pcl::PCLPointCloud2 cloud_upper_output;
+     // sensor_msgs::PointCloud2 msg_lower_output;
+     // sensor_msgs::PointCloud2 msg_mid_output;
+     // sensor_msgs::PointCloud2 msg_upper_output;
 
-     // Create the filtering object
-     pcl::PassThrough<pcl::PointXYZ> pass;
-     pass.setInputCloud(cloud);
-     pass.setFilterFieldName("z");
-     pass.setFilterLimits(0.0, 1.0);
-     //pass.setFilterLimitsNegative (true);
-     pass.filter(*cloud_filtered);
+     // // Create the filtering object
+     // pcl::PassThrough<pcl::PointXYZ> pass;
+     // pass.setInputCloud(cloud);
+     // pass.setFilterFieldName("z");
+     // pass.setFilterLimits(0.0, 1.0);
+     // //pass.setFilterLimitsNegative (true);
+     // pass.filter(*cloud_filtered);
 
-     pcl::toPCLPointCloud2(*cloud_filtered, cloud_lower_output);
-     pcl_conversions::fromPCL(cloud_lower_output, msg_lower_output);
-     pub_lower.publish(msg_lower_output);
+     // pcl::toPCLPointCloud2(*cloud_filtered, cloud_lower_output);
+     // pcl_conversions::fromPCL(cloud_lower_output, msg_lower_output);
+     // pub_lower.publish(msg_lower_output);
 
-     // std::cerr << "Cloud after filtering: " << std::endl;
-     // for (std::size_t i = 0; i < cloud_filtered->points.size(); ++i)
-     //      std::cerr << "    " << cloud_filtered->points[i].x << " "
-     //                << cloud_filtered->points[i].y << " "
-     //                << cloud_filtered->points[i].z << std::endl;
+     // // std::cerr << "Cloud after filtering: " << std::endl;
+     // // for (std::size_t i = 0; i < cloud_filtered->points.size(); ++i)
+     // //      std::cerr << "    " << cloud_filtered->points[i].x << " "
+     // //                << cloud_filtered->points[i].y << " "
+     // //                << cloud_filtered->points[i].z << std::endl;
 // filtering a PointCloud using a PassThrough filter%
 
 // for debugging%
