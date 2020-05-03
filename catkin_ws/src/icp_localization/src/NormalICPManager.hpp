1123fda30089734465d0ca0eefddce0af3f5fc7c
biomotion <bensonpuppy1997@gmail.com>
2020-05-03 12:34:14 +0800

    create icp with normal just give it a try

---
+++ catkin_ws/src/icp_localization/src/NormalICPManager.hpp
@@ -0,0 +1,53 @@
+#include<pcl_conversions/pcl_conversions.h>
+#include <pcl/registration/icp.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/features/normal_3d.h>
+#include <iostream>
+
+using namespace pcl;
+
+#ifndef NORMAL_ICP_MANAGER_H
+#define NORMAL_ICP_MANAGER_H
+class NormalICPManager{
+private:
+    Eigen::Matrix4Xd pose, guess;
+    // PCL stuffs
+    pcl::IterativeClosestPointWithNormals<PointXYZRGBNormal, PointXYZRGBNormal, double> icp;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr map_cloud;
+    pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr map_with_normals;
+    void selectMapRange(float, float, float, float, float, float, pcl::PointCloud<PointXYZRGBNormal>::Ptr&);
+    void computeNormal(pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr);
+public:
+    NormalICPManager();
+    NormalICPManager(char const * map_file);
+    void loadMap(std::string);
+    void feedPC(pcl::PointCloud<pcl::PointXYZ>::Ptr&);
+    // void feedPC(pcl::PointCloud<pcl::PointXYZ>, Eigen::Matrix4d); //feed point cloud with guess
+    void guessTF(Eigen::Matrix4d g) { this->guess = g; }
+    void guessOrientation(Eigen::Quaterniond q) { guessOrientation(q.toRotationMatrix()); } // guess orientation by quaternion
+    void guessOrientation(Eigen::Matrix3d orien){ // guess orientation by rotation matrix
+        if(this->guess.topRightCorner(3, 1).isApprox(Eigen::Vector3d::Zero()))
+            this->guess = this->pose;
+        this->guess.topLeftCorner(3, 3) = orien;
+        // std::cout << "guess orientation\n" << orien << std::endl;
+    }
+    void guessPosition(Eigen::Vector3d posi){
+        if(this->guess.topLeftCorner(3, 3).isApprox(Eigen::Matrix3d::Identity()))
+            this->guess = this->pose;
+        this->guess.topRightCorner(3, 1) = posi;
+        // std::cout << "guess position\n" << posi << std::endl;
+    }
+    Eigen::Matrix4d getPose() { return this->pose; }
+    void setParams(double max_d, double tf_esln, double fit_esln, uint16_t max_iter){
+        this->icp.setMaxCorrespondenceDistance(max_d);
+        this->icp.setTransformationEpsilon(tf_esln);
+        this->icp.setEuclideanFitnessEpsilon(fit_esln);
+        this->icp.setMaximumIterations(max_iter);
+    }
+
+};
+
+#endif
